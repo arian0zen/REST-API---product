db20d127c51b4f60564611ac6ee77bb310705548
Arian Shaikh <ariansk90@gmail.com>
2022-09-02 12:57:02 +0530

    added readme.md and product specific get route

--- app.js
+++ app.js
@@ -2,15 +2,15 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require('mongoose');
-
+const _ = require('lodash');
 const app = express();
-
 app.set('view engine', 'ejs');
-
 
 app.use(bodyParser.urlencoded({
   extended: true
 }));
+
+
 app.use(bodyParser.json());
 app.use(express.static("public"));
 
@@ -27,10 +27,12 @@ const productSchema = {
 
 const Product = mongoose.model("Product", productSchema);
 
+
+// get route endpoints
+
 app.get('/', (req, res) => {
     res.render("home");
 });
-
 
 app.get('/add', (req, res) => {
     res.render("add");
@@ -42,15 +44,37 @@ app.get("/showproducts", (req, res) => {
         if(err){
             console.log("Product not found", err);
         } else{
-            console.log(product);
             res.json(product);
         }
     })
 } );
 
+
+app.get("/showproducts/:title", (req, res)=>{
+    const customTitle = _.capitalize(req.params.title);
+    Product.findOne({title: customTitle}, (err, foundItem)=>{
+        if(!err){
+            if(!foundItem){
+                res.json({
+                    statusCode: 404,
+                    message: "Not found"
+
+                })
+            } else{
+                res.json(foundItem)
+            }
+        } else{
+            console.log(err)
+        }
+    })
+})
+
+
+// post route endpoints
+
 app.post("/products", (req, res) =>{
     const newProduct = new Product({
-        title: req.body.productName,
+        title: _.capitalize(req.body.productName),
         id:  req.body.productId,
         description: req.body.productDesc,
         price: req.body.productPrice,
@@ -66,7 +90,7 @@ app.post("/products", (req, res) =>{
                 message: "Adding product failed"
             })
         } else{
-            console.log(req.body.productName);
+
             res.json({
                 response_code: 201,
                 status: "Created",
@@ -77,8 +101,7 @@ app.post("/products", (req, res) =>{
 });
 
 
-
-app.listen(80, () => {
+const PORT = process.env.PORT || 80;
+app.listen(PORT, () => {
     console.log("listening on port 80");
   });
-  
